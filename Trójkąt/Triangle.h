3570d1c448f4376a024ecdc2ab3dbd7f8c845a90
Mateusz Kubiak <mati.kub@interia.pl>
2020-03-03 18:31:07 +0100

    Skończony projekt

---
+++ Trójkąt/Triangle.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/**
+ * Tworzy klasê triangle zawieraj¹c¹ 
+ * nazwê i parametry trójk¹ta.
+ * @param a pierwszy bok
+ * @param b drugi bok
+ * @param c trzeci bok
+ * @param name nazwa trójk¹ta
+ * @fn circuit zwraca sumê boków
+ */
+class Triangle {
+public:
+    string name;
+    int a, b, c;
+    int circuit() { return a + b + c; }
+};
+
+/**
+ * Przeci¹¿enie operatora << dla obiektu klasy triangle.
+ * @param trójk¹t
+ * @return parametry trójk¹ta
+*/
+ostream& operator << (ostream& strm, Triangle T)
+{
+    strm << "Nazwa: " << T.name << endl;
+    //strm << "Bok a: " << T.a << endl;
+    //strm << "Bok b: " << T.b << endl;
+    //strm << "Bok c: " << T.c << endl;
+    strm << "Obwod: " << T.circuit() << endl << endl;
+    return strm;
+}
---
+++ Trójkąt/Trójkąt.cpp
@@ -0,0 +1,32 @@
+﻿/**
+ * Trójkąt.cpp
+ * Purpose: Czytanie parametrów trójkątów z pliku
+ * i wyświetlanie ich
+ * @author Mateusz Kubiak
+ * @version 1.0 02/03/2020
+ */
+#include <fstream>
+#include <string>
+#include <vector>
+#include "Triangle.h"
+
+int main() 
+{
+    vector<Triangle> triangleVector;
+    Triangle triangle;
+    ifstream file;
+    file.open("Trojkaty1.txt");
+    if (!file) 
+    {
+        cerr << "Nie ma takiego pliku" << endl;
+        return 1;
+    }
+    while (!file.eof()) 
+    {
+        file >> triangle.name >> triangle.a >> triangle.b >> triangle.c;
+        triangleVector.push_back(triangle);
+    }
+    for (int i = 0; i < triangleVector.size(); i++) cout << triangleVector[i];
+    file.close();
+    return 0;
+}
